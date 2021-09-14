4ca69f76ae0dd2beb54d8de72161ff531edd3f50
Marcus Lee Eugene <marcus19787@gmail.com>
2021-09-14 21:48:58 +0800

    Add in scribble background

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -12,23 +12,30 @@ import styles from './Login.module.css';
 
 //Resources
 import NUSociaLifeLogo from '../../resources/NUSociaLife_Login_Logo.png';
+import RandomScribble from '../../resources/RandomScribble.png';
 
 function Login() {
     return (
-        <Container>
-            <Grid container 
-                alignItems="center"
-                justify="center"
-                className={styles.verticalCenter}
-            >
-                <Grid md={6}>
-                    <img alt='NUSociaLifeLogo' src={NUSociaLifeLogo}/>
+        <div>
+            <Container>
+                <Grid container 
+                    alignItems="center"
+                    justifyContent="center"
+                    className={styles.verticalCenter}
+                >
+                    <Grid item md={6}>
+                        <img alt='NUSociaLifeLogo' src={NUSociaLifeLogo}/>
+                    </Grid>
+                    <Grid item md={6}>
+                        <LoginForm/>
+                    </Grid>
                 </Grid>
-                <Grid md={6}>
-                    <LoginForm/>
-                </Grid>
+            </Container>
+            <Grid item md={12}>
+                <img alt='RandomScribble' src={RandomScribble} className={styles.randomScribble}/>
             </Grid>
-        </Container>
+        </div>
+        
     )
 }
 
