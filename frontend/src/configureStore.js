f22b2b2cbc06b38e8eee6777858979a4738ea4a2
cheeerynne <cheeerynne@gmail.com>
2021-09-20 18:17:41 +0800

    Setup Redux and login logic

---
+++ frontend/src/configureStore.js
@@ -0,0 +1,24 @@
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage' 
+import { persistStore, persistReducer } from 'redux-persist'
+
+export default function configureStore(persistedState) {
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const store = createStore(
+    persistedReducer,
+    persistedState,
+    composeWithDevTools(  
+      applyMiddleware(thunkMiddleware)
+    ),
+  );
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
+import App from './App';
+import configureStore from "./configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import './index.css';
-import App from './App';
 import * as serviceWorker from "./serviceWorker";
 
+const store = configureStore();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 
