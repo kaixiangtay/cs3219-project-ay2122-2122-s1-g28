3812ccdacb073e3e3cec9fd01ca53ce654c2541e
cheeerynne <cheeerynne@gmail.com>
2021-09-24 20:50:05 +0800

    Implement logout functionality

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -3,6 +3,9 @@ import { toast } from "react-toastify";
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "LOGOUT_FAILURE"; 
 
 const loginRequest = () => {
   return {
@@ -25,6 +28,25 @@ const loginFailure = () => {
     type: LOGIN_FAILURE
   };
 };
+
+const logoutRequest = () => { 
+  return { 
+    type: LOGOUT_REQUEST
+  };
+}; 
+
+const logoutSuccess = (props) => {
+  props.history.push('/login');
+  return { 
+    type: LOGOUT_SUCCESS
+  };
+}; 
+
+const logoutFailure = () => { 
+  return { 
+    type: LOGOUT_FAILURE
+  };
+}; 
 
 export const loginUser = (email, password) => dispatch => {
   const requestUrl = ``;
@@ -51,3 +73,13 @@ export const loginUser = (email, password) => dispatch => {
       dispatch(loginFailure());
     });
 };
+
+export const logoutUser = (props) => dispatch => { 
+  dispatch(logoutRequest());
+  try { 
+    //Remove auth token here, if successful, dispatch logoutSuccess 
+    dispatch(logoutSuccess(props));
+  } catch(err) {
+    dispatch(logoutFailure());
+  }
+}; 
