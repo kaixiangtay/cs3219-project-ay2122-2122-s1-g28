e1d0fcb13dedf8c7d27d34f524f69f7bfc366837
Marcus Lee Eugene <marcus19787@gmail.com>
2021-10-09 01:22:11 +0800

    Add in redux for navigation of navigation bar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,10 @@
 // Import Settings
-import React, { useEffect, useState } from 'react';
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
+// Import Redux
+import { handleNavigation } from '../../actions/navigation';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Import Material-ui
 import { 
@@ -18,57 +21,70 @@ import { faCommentAlt, faUserCircle } from '@fortawesome/free-regular-svg-icons'
 // Import CSS
 import styles from './Navbar.module.css';
 
+// Import constants
+import { 
+    FINDFRIENDS, 
+    FORUM, 
+    PROFILE,
+    LOGOUT
+} from '../../constants/ReduxConstants.js';
+
 // Import Resources
 import NUSociaLifeLogo from '../../resources/NUSociaLife_Navbar_Icon.png';
 
 // Import Actions
-import { logoutUser } from '../../actions';
+// import { logoutUser } from '../../actions';
 
-function Navbar(props) {
-    const { handleLogoutRequest } = props; 
-    const [selection, setSelection] = useState('Find Friends')
+function Navbar() {
+    const history = useHistory();
+    const navigation = useSelector(state => state.navigation);
+    const dispatch = useDispatch();
+
+    const handleSelection = (location) => { 
+        dispatch(handleNavigation(location));
+    };
+
     const navItems = [
         {
             icon: faUserFriends,
             title: 'Find Friends',
-            // path: '/findfriends'
+            location: FINDFRIENDS
         },
         {
             icon: faCommentAlt,
             title: 'Forum',
+            location: FORUM
         },
         {
             icon: faUserCircle,
             title: 'Profile',
-            // path: '/profile'
+            location: PROFILE
         },
         {
             icon: faSignOutAlt,
-            title: 'Logout'
+            title: 'Logout',
+            location: LOGOUT
         },
     ];
-    const handleSelection = (title) => { 
-        setSelection(title);
 
-        if (title === 'Logout') { 
-            handleLogoutRequest(); 
-        } else if (title === 'Forum') { 
-            // props.history.push('/forum');
-        } else if (title === 'Profile') { 
-            props.history.push('/profile');
-        } else if (title === 'Find Friends') {
-            props.history.push('/findfriends');
-        } else {
-            return; 
+    useEffect(() => {
+        switch(navigation.selection) {
+            case FINDFRIENDS:
+                history.push('/findfriends');
+                break;
+            case FORUM:
+                history.push('/forum');
+                break;
+            case PROFILE:
+                history.push('/profile');
+                break;
+            case LOGOUT:
+                history.push('/login');
+                break;
+            default:
+                history.push('/findfriends');
         }
-    }; 
-
-    // Handle logout request
-    useEffect(() => { 
-        if (selection === 'Logout') { 
-            handleLogoutRequest(); 
-        }
-    }); 
+    }, [history, navigation])
 
     return (
         <AppBar position="static" className={styles.navColor}>
@@ -82,11 +98,11 @@ function Navbar(props) {
                             <IconButton 
                                 key={item.title} 
                                 color='inherit' 
-                                className={selection === item.title 
+                                className={navigation.selection === item.location 
                                     ? styles.selected 
                                     : null
                                 }
-                                onClick={() => handleSelection(item.title)}
+                                onClick={() => handleSelection(item.location)}
                             >
                                 <div className={styles.navIcon}>
                                     <FontAwesomeIcon icon={item.icon} />
@@ -103,16 +119,4 @@ function Navbar(props) {
     )
 }
 
-function mapStateToProps() { 
- return {};
-}
-
-function mapDispatchToProps(dispatch, props) { 
-    return {
-        handleLogoutRequest: () => dispatch(logoutUser(props))
-    };
-}
-
-const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-export default withRouter(withConnect(Navbar));
+export default Navbar;
--- frontend/src/constants/ReduxConstants.js
+++ frontend/src/constants/ReduxConstants.js
@@ -5,3 +5,9 @@ export const RESET = 3;
 
 // Signup
 export const VERIFIED = 4;
+
+// Navigation
+export const FINDFRIENDS = 10;
+export const FORUM = 11;
+export const PROFILE= 12;
+export const LOGOUT = 13;
