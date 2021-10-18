32dd7ae3f690260cd76ee4103233da589a7ea642
kaixiangtay <taykaixiang97@gmail.com>
2021-10-18 21:17:29 +0800

    Refactor code and improvise resetPassword functionality

--- backend/users/src/controllers/userController.js
+++ backend/users/src/controllers/userController.js
@@ -1,26 +1,18 @@
-const {
-  EMAIL,
-  PORT,
-  FRONTEND_URL,
-  S3_BUCKET_NAME,
-} = require("../config/config");
-
-var User = require("../models/userModel");
-var userAuth = require("../middlewares/userAuth");
-const mailerService = require("../services/mailerService");
-const imagerService = require("../services/imageService");
+const { S3_BUCKET_NAME } = require("../config/config");
+const userAuth = require("../middlewares/userAuth");
+const userDb = require("../middlewares/userDb");
+const imageService = require("../services/imageService");
 const { validationResult } = require("express-validator");
-const {
-  userRegisterValidator,
-  userLoginValidator,
-  userUpdateValidator,
-} = require("../middlewares/userValidator");
+const { userRegisterValidator, userLoginValidator, userUpdateValidator } = require("../middlewares/userValidator");
 
-exports.index = function (req, res) {
-  User.get(function (err, users) {
-    if (err) {
-      return res.status(404).json({
-        status: "error",
+exports.index = async (req, res) => {
+  try {
+    const users = await userDb.getAllUsers();
+    const emptyUserDatabase = users.length == 0;
+
+    if (emptyUserDatabase) {
+      return res.status(200).json({
+        status: "success",
         msg: "No Users in database found",
       });
     } else {
@@ -30,162 +22,143 @@ exports.index = function (req, res) {
         data: users,
       });
     }
-  });
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
 };
 
-exports.registerUser = [
-  userRegisterValidator(),
-  (req, res) => {
+// Register new user
+exports.registerUser = [userRegisterValidator(), async (req, res) => {
+  try {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
       return res.status(404).json(errors.array());
-    } else {
-      User.findOne({ email: req.body.email }).then((user) => {
-        if (user) {
-          return res.status(404).json([
-            {
-              status: "error",
-              msg: "Email already exists!",
-            },
-          ]);
-        } else {
-          var user = new User();
-          user.name = req.body.name;
-          user.email = req.body.email;
-          user.password = userAuth.hashPassword(req.body.password);
-          user.token = userAuth.createAccessToken(user.email);
-          user.save();
+    } 
 
-          mailerService.sendEmail({
-            from: EMAIL,
-            to: user.email,
-            subject: "NUSociaLife Account Verification",
-            html: `<p>Click <a href="${FRONTEND_URL}/verify-email/${user.token}">here</a> to activate your account. Note: Link is only valid for 15 minutes!!!</p>`,
-          });
+    let user = await userDb.getUserByEmail(req.body.email);
 
-          return res.status(200).json({
-            status: "success",
-            msg: "New user created!",
-            data: user,
-          });
-        }
+    if (user) {
+      return res.status(404).json([{
+          status: "error",
+          msg: "Email already exists!",
+      }]);
+    }
+     else {
+      user = userDb.createUser(req.body);
+      
+      return res.status(200).json({
+        status: "success",
+        msg: "New user created!",
+        data: user,
       });
     }
-  },
-];
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
+}];
+
 
 // Resend verification email when token has expired after 15 mins
-exports.resendActivationEmail = function (req, res) {
-  User.findOne({ email: req.body.email }, function (err, user) {
-    user.token = userAuth.createAccessToken(user.email);
+exports.resendActivationEmail = async (req, res) => {
+  try {
+    let user = await userDb.getUserByEmail(req.body.email);
 
-    user.save(function (err) {
-      if (err) {
-        return res.status(404).json(err);
-      } else {
-        mailerService.sendEmail({
-          from: EMAIL,
-          to: user.email,
-          subject: "NUSociaLife Account Verification",
-          html: `<p>Click <a href="${FRONTEND_URL}/verify-email/${user.token}">here</a> to activate your account. Note: Link is only valid for 15 minutes!!!</p>`,
-        });
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        msg: "Invalid email detected!",
+      });
+    } 
 
-        return res.status(200).json({
-          status: "success",
-          msg: "New account sign up email link sent!",
-        });
-      }
+    if (user.isVerified) {
+      return res.status(404).json({
+        status: "error",
+        msg: "Account has been verified!",
+      });
+    }
+
+    userDb.resendEmail(user);
+
+    return res.status(200).json({
+      status: "success",
+      msg: "New account sign up email link have been resend!",
     });
-  });
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
 };
 
 // API to verify user email from email link
-exports.verifyUserEmail = function (req, res) {
-  User.findOne({ token: req.params.token }, function (err, user) {
+exports.verifyUserEmail = async (req, res) => {
+  try {
+    let user = await userDb.getUserByToken(req.params.token);
     const userEmail = userAuth.decodeTempToken(user.token);
 
-    if (userEmail === user.email) {
-      user.status = "Approved";
-      user.save();
-
-      return res.status(200).json({
-        status: "success",
-        msg: "Your email has been verified",
-        data: user,
-      });
-    } else {
+    if (userEmail !== user.email) {
       return res.status(404).json({
         status: "error",
         msg: "Link has expired!",
       });
-    }
-  });
-};
+    } 
 
-// Send email for user to reset password
-exports.sendResetPasswordEmail = function (req, res) {
-  User.findOne({ email: req.body.email }, function (err, user) {
-    user.token = userAuth.resetPasswordToken(user.email);
-
-    user.save(function (err) {
-      if (err) {
-        return res.status(404).json(err);
-      } else {
-        mailerService.sendEmail({
-          from: EMAIL,
-          to: user.email,
-          subject: "NUSociaLife Account Verification",
-          html:
-            '<p>Click <a href="http://localhost:' +
-            PORT +
-            "/api/users/resetPassword/" +
-            user.token +
-            '">here</a> to reset your password. Note: Link is only valid for 15 minutes!!!</p>',
-        });
-
-        return res.status(200).json({
-          status: "success",
-          msg: "Reset password email link sent!",
-          data: user,
-        });
-      }
-    });
-  });
-};
-
-// Reset password when user not logged in to account (will use the same if password link expires)
-exports.resetPassword = function (req, res) {
-  User.findOne({ token: req.params.token }, function (err, user) {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(404).json(errors.array());
-    }
-
-    user.password = userAuth.hashPassword(req.body.password);
-    user.save();
+    user = userDb.verifyUser(user);
 
     return res.status(200).json({
       status: "success",
-      msg: "User password has successfully been reset!",
+      msg: "Your email has been verified",
       data: user,
     });
-  });
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
+}
+
+// Send email for user to reset password
+exports.resetPassword = async (req, res) => {
+  try {
+    let user = await userDb.getUserByEmail(req.body.email);
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        msg: "Invalid email detected!",
+      });
+    } else {
+      userDb.resetPassword(user);
+      
+      return res.status(200).json({
+        status: "success",
+        msg: "Reset password email link with temporary password sent!",
+      });
+    }
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
 };
 
 // Upload Image into AWS S3 bucket
-exports.uploadProfileImage = [
-  userAuth.authenticateToken,
-  (req, res) => {
-    const authHeader = req.headers["authorization"];
-    var userID = userAuth.decodeAuthToken(authHeader);
+exports.uploadProfileImage = [userAuth.authenticateToken, async (req, res) => {
+    try {
+      const authHeader = req.headers["authorization"];
+      let user = await userDb.getUserByID(authHeader);
 
-    User.findById(userID, function (err, user) {
       // frontend file name put to profileImage
-      const uploadSingleImage = imagerService
-        .upload(S3_BUCKET_NAME, user._id)
-        .single("profileImage");
+      const uploadSingleImage = imageService.upload(S3_BUCKET_NAME, user._id).single("profileImage");
 
       uploadSingleImage(req, res, async (err) => {
         if (err) {
@@ -194,9 +167,8 @@ exports.uploadProfileImage = [
             msg: "Invalid file type, must be an image file!",
           });
         } else {
-          user.profileImageUrl = req.file.location;
-          user.save();
-
+          user = userDb.saveProfileImageUrl(user, req.file.location);
+     
           return res.status(200).json({
             status: "success",
             msg: "Profile uploaded successfully!",
@@ -207,33 +179,25 @@ exports.uploadProfileImage = [
             },
           });
         }
+      })
+    } catch (err) {
+      return res.status(400).json({ 
+        status: "error",
+        msg: err.toString(),
       });
-    });
-  },
-];
+    }
+}];
 
 // Change user name and password in Profile page when user logged in to account
-exports.updateUser = [
-  userAuth.authenticateToken,
-  userUpdateValidator(),
-  (req, res) => {
-    const authHeader = req.headers["authorization"];
-    var userID = userAuth.decodeAuthToken(authHeader);
-
-    User.findById(userID, function (err, user) {
+exports.updateUser = [userAuth.authenticateToken, userUpdateValidator(), async (req, res) => {
+    try {
       const errors = validationResult(req);
-
       if (!errors.isEmpty()) {
         return res.status(404).json(errors.array());
-      }
-
-      user.name = req.body.name;
-
-      if (req.body.password !== undefined) {
-        user.password = userAuth.hashPassword(req.body.password);
-      }
-
-      user.save();
+      } 
+      
+      const authHeader = req.headers["authorization"];
+      let user = await userDb.updateUser(authHeader, req.body);
 
       return res.status(200).json({
         status: "success",
@@ -243,118 +207,124 @@ exports.updateUser = [
           email: user.email,
           profileImageUrl: user.profileImageUrl,
         },
-      });
-    });
-  },
-];
-
-exports.viewUser = [
-  userAuth.authenticateToken,
-  (req, res) => {
-    const authHeader = req.headers["authorization"];
-    var userID = userAuth.decodeAuthToken(authHeader);
-
-    User.findById(userID, function (err, user) {
-      return res.status(200).json({
-        status: "success",
-        msg: "User details loading..",
-        data: {
-          name: user.name,
-          email: user.email,
-          profileImageUrl: user.profileImageUrl,
-        },
-      });
-    });
-  },
-];
-
-exports.deleteUser = [
-  userAuth.authenticateToken,
-  (req, res) => {
-    const authHeader = req.headers["authorization"];
-    var userID = userAuth.decodeAuthToken(authHeader);
-
-    User.findById(userID, function (err, user) {
-      if (user.profileImageUrl !== "") {
-        imagerService.delete(user.profileImageUrl);
-      }
-
-      User.deleteOne({ _id: user._id }, function (err, user) {
-        return res.status(200).json({
-          status: "success",
-          msg: "Account deleted successfully",
-        });
-      });
-    });
-  },
-];
-
-exports.loginUser = [
-  userLoginValidator(),
-  (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(404).json(errors.array());
-    } else {
-      User.findOne({ email: req.body.email }).then((user) => {
-        if (!user) {
-          return res.status(404).json({
-            status: "error",
-            msg: "Email cannot be found!",
-          });
-        } else {
-          if (user.status !== "Approved") {
-            return res.status(400).json({
-              status: "error",
-              msg: "User is not verified, unable to login!",
-            });
-          }
-          const body = req.body;
-
-          // compare user password with hashed password in database
-          const validPassword = userAuth.comparePassword(
-            body.password,
-            user.password
-          );
-
-          if (validPassword) {
-            // Allow token access for a day
-            user.token = userAuth.createLoginToken(user._id);
-            user.save();
-
-            return res.status(200).json({
-              token: user.token,
-              status: "success",
-              msg: "Login successful!",
-            });
-          } else {
-            return res.status(400).json({
-              status: "error",
-              msg: "Invalid Password!",
-            });
-          }
-        }
+      });  
+    } catch (err) {
+      return res.status(400).json({ 
+        status: "error",
+        msg: err.toString(),
       });
     }
-  },
-];
+}];
 
-exports.logout = [
-  userAuth.authenticateToken,
-  (req, res) => {
+// View user profile
+exports.viewUser = [userAuth.authenticateToken, async (req, res) => {
+  try {
     const authHeader = req.headers["authorization"];
-    var userID = userAuth.decodeAuthToken(authHeader);
+    let user = await userDb.getUserByID(authHeader);
+    
+    return res.status(200).json({
+      status: "success",
+      msg:  "User details loading..",
+      data: {
+        name: user.name,
+        email: user.email,
+        profileImageUrl: user.profileImageUrl,
+      },
+    });       
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
+}];
 
-    User.findById(userID, function (err, user) {
-      // Clear token before logout
-      user.token = "";
-      user.save();
+exports.deleteUser = [userAuth.authenticateToken, async (req, res) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    // Database will return delete count of data
+    const deletedCount = await userDb.deleteUser(authHeader);;
+    const isDeletedUser = deletedCount == 1; 
 
+    if (isDeletedUser) {
       return res.status(200).json({
         status: "success",
-        msg: "Have a nice day!",
+        msg: "Account deleted successfully",
       });
+    }
+    
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
     });
-  },
-];
+  }
+}];
+
+exports.loginUser = [userLoginValidator(), async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json(errors.array());
+    }
+
+    let user = await userDb.getUserByEmail(req.body.email);
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        msg: "Email cannot be found!",
+      });
+    } 
+
+     // compare user password with hashed password in database
+     const validPassword = userAuth.comparePassword(req.body.password, user.password);
+
+     if (!validPassword) {
+      return res.status(400).json({
+        status: "error",
+        msg: "Invalid Password!",
+      });
+    }
+
+    if (!user.isVerified) {
+      return res.status(400).json({
+        status: "error",
+        msg: "User is not verified, unable to login!",
+      });
+    }
+
+    user = userDb.loginUser(user);
+
+    return res.status(200).json({
+      token: user.token,
+      status: "success",
+      msg: "Login successful!",
+    });
+
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
+}];
+
+exports.logout = [userAuth.authenticateToken, async (req, res) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    let user = await userDb.getUserByID(authHeader);
+
+    userDb.logoutUser(user);
+
+    return res.status(200).json({
+      status: "success",
+      msg: "Have a nice day!",
+    });
+  } catch (err) {
+    return res.status(400).json({ 
+      status: "error",
+      msg: err.toString(),
+    });
+  }
+}];
--- backend/users/src/services/mailerService.js
+++ backend/users/src/services/mailerService.js
@@ -1,4 +1,4 @@
-const { GCP_CLIENT_ID, GCP_CLIENT_SECRET, EMAIL, GCP_ACCESS_TOKEN, GCP_REFRESH_TOKEN, OAUTH_URL } = require('../config/config')
+const { GCP_CLIENT_ID, GCP_CLIENT_SECRET, EMAIL, GCP_ACCESS_TOKEN, GCP_REFRESH_TOKEN, FRONTEND_URL } = require('../config/config');
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
@@ -15,10 +15,53 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-exports.sendEmail = async (emailOptions) => {
+const registerAccountEmailOptions = (email, token) => {
+  const emailOptions = {
+    from: EMAIL,
+    to: email,
+    subject: "NUSociaLife Account Verification",
+    // html: `<p>Click <a href="${FRONTEND_URL}/verify-email/${token}">here</a> to activate your account. Note: Link is only valid for 15 minutes!!!</p>`,
+    html: `<p>Click <a href="${'http://localhost:5000/api/users'}/verifyEmail/${token}">here</a> to activate your account. Note: Link is only valid for 15 minutes!!!</p>`,
+  };
+
+  return emailOptions;
+} 
+
+const resetPasswordEmailOptions = (email, tempPassword) => {
+  const emailOptions = {
+    from: EMAIL,
+    to: email,
+    subject: "NUSociaLife Account Reset Passsword",
+    html: `<p>Here is your temporary password: ${tempPassword} </p>`,
+  };
+
+  return emailOptions;
+} 
+
+exports.sendRegisterUserEmail = async (email, token) => {
     try {
+      const emailOptions = registerAccountEmailOptions(email, token);
       await transporter.sendMail(emailOptions);
     } catch (err) {
         console.log(err);
+
+        return res.status(404).json([{
+          status: "error",
+          msg: "Unable to connect to Nodemailer!",
+        }])
+    }
+  };
+
+exports.sendForgotPasswordEmail = async (email, tempPassword) => {
+    try {
+      const emailOptions = resetPasswordEmailOptions(email, tempPassword);
+      await transporter.sendMail(emailOptions);
+    } catch (err) {
+        console.log(err);
+
+        return res.status(404).json([{
+          status: "error",
+          msg: "Unable to connect to Nodemailer!",
+        }])
     }
   };
