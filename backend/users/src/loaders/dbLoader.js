6a3ca3a90a824206fb769d9512dba73be6bd88a3
rachelljt <rachel.ljt@gmail.com>
2021-10-26 20:44:57 +0800

    Check code style using eslint for users ms

--- backend/users/src/loaders/dbLoader.js
+++ backend/users/src/loaders/dbLoader.js
@@ -1,25 +1,27 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require("mongoose");
-const { DB_URL } = require('../config/config')
+const { DB_URL } = require("../config/config");
 
-const username = "user"; //username for admin
-const password = "users123"; //password for admin
-const cluster = "Cluster0"; //cluster number
-const dbname = "UserDatabase"; //database name for users
+// eslint-disable-next-line no-unused-vars
+const username = "user"; // username for admin
+const password = "users123"; // password for admin
+const cluster = "Cluster0"; // cluster number
+const dbname = "UserDatabase"; // database name for users
 
 mongoose.Promise = global.Promise;
 
-const connection = mongoose.connect(DB_URL,   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+const connection = mongoose.connect(DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 connection
-	.then(db => {
+	.then((db) => {
 		console.log("Database connection successful");
 		return db;
 	})
-	.catch(err => {
-		if (err.message.code === 'ETIMEDOUT') {
+	.catch((err) => {
+		if (err.message.code === "ETIMEDOUT") {
 			console.log("Attempting to re-establish database connection");
 			mongoose.connect(DB_URL);
 		} else {
--- backend/users/src/middlewares/userValidator.js
+++ backend/users/src/middlewares/userValidator.js
@@ -1,51 +1,51 @@
-const { check } = require('express-validator')
+const { check } = require("express-validator");
 
 const userNameValidator = () => {
-    return [
-        check('name').notEmpty().withMessage("Name is a compulsory field"),
-    ]
-}
+	return [
+		check("name").notEmpty().withMessage("Name is a compulsory field"),
+	];
+};
 
 const userEmailValidator = () => {
-    return [
-    check('email')
-    .custom(value => {
-        const domain = value.substring(value.lastIndexOf("@") + 1)
-        if (domain === "u.nus.edu") {
-            return true
-        } else {
-            return false
-        }
-    })
-    .withMessage("Email should be in the form of @u.nus.edu"),
-    ]
-}
+	return [
+		check("email")
+			.custom((value) => {
+				const domain = value.substring(value.lastIndexOf("@") + 1);
+				if (domain === "u.nus.edu") {
+					return true;
+				} else {
+					return false;
+				}
+			})
+			.withMessage("Email should be in the form of @u.nus.edu"),
+	];
+};
 
 const userPasswordValidator = () => {
-    return [
-        check('password').isStrongPassword().withMessage("Password should be of minimum length 8, consists of 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"),
-    ]
-}
+	return [
+		check("password").isStrongPassword().withMessage("Password should be of minimum length 8, consists of 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"),
+	];
+};
 
 const userRegisterValidator = () => {
-    return [
-       userNameValidator(),
-       userEmailValidator(),
-       userPasswordValidator(),
-    ]
+	return [
+		userNameValidator(),
+		userEmailValidator(),
+		userPasswordValidator(),
+	];
 };
 
 const userLoginValidator = () => {
-    return [
-        userEmailValidator(),
-        userPasswordValidator(),
-     ]
-}
+	return [
+		userEmailValidator(),
+		userPasswordValidator(),
+	];
+};
 
 const userUpdateValidator = () => {
-    return [
-        check('password').optional().isStrongPassword().withMessage("Password should be of minimum length 8, consists of 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"),
-    ]
-}
+	return [
+		check("password").optional().isStrongPassword().withMessage("Password should be of minimum length 8, consists of 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"),
+	];
+};
 
 module.exports = { userRegisterValidator, userLoginValidator, userUpdateValidator };
