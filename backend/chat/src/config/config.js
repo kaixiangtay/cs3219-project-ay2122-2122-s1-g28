411de5df5e18f5870de2dedb9e6d93e7975597ed
Marcus Lee Eugene <marcus19787@gmail.com>
2021-10-31 19:20:07 +0800

    Refactor all env variables, refactor dockercompose to allow for github secrets

--- backend/chat/src/config/config.js
+++ backend/chat/src/config/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.CHAT_PORT;
 
 export { PORT };
--- backend/forum/src/config/config.js
+++ backend/forum/src/config/config.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 // dotenv.config();
 
-const DB_URL = process.env.DB_URL;
-const PORT = process.env.PORT;
+const DB_URL = process.env.FORUM_DB_URL;
+const PORT = process.env.FORUM_PORT;
 const JWT_ACCESS_TOKEN = process.env.JWT_ACCESS_TOKEN;
 
 export { DB_URL, PORT, JWT_ACCESS_TOKEN };
