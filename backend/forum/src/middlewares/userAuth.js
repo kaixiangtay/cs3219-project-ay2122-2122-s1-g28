79cb48a76fcfd641d513e8d413974e0b2398f3d8
rachelljt <rachel.ljt@gmail.com>
2021-11-02 13:39:21 +0800

    Refactor Forum Eslint files

--- backend/forum/src/middlewares/userAuth.js
+++ backend/forum/src/middlewares/userAuth.js
@@ -2,31 +2,31 @@ import jwt from "jsonwebtoken";
 import { JWT_ACCESS_TOKEN } from "../config/config.js";
 
 function getToken(header) {
-  const token = header && header.split(" ")[1];
-  return token;
+	const token = header && header.split(" ")[1];
+	return token;
 }
 
 function decodeAuthToken(req, res, next) {
-  const token = getToken(req.headers["authorization"]);
+	const token = getToken(req.headers["authorization"]);
 
-  if (!token) {
-    // No token in req header
-    return res.status(401).json({
-      success: "error",
-      msg: "Unauthorized to access data",
-    });
-  }
+	if (!token) {
+		// No token in req header
+		return res.status(401).json({
+			success: "error",
+			msg: "Unauthorized to access data",
+		});
+	}
 
-  try {
-    const decoded = jwt.verify(token, JWT_ACCESS_TOKEN);
-    req.userId = decoded._id;
-    return next();
-  } catch (error) {
-    // Invalid token detected in req header
-    return res.status(401).json({
-      success: "error",
-      msg: "Unauthorized to access data",
-    });
-  }
+	try {
+		const decoded = jwt.verify(token, JWT_ACCESS_TOKEN);
+		req.userId = decoded._id;
+		return next();
+	} catch (error) {
+		// Invalid token detected in req header
+		return res.status(401).json({
+			success: "error",
+			msg: "Unauthorized to access data",
+		});
+	}
 }
 export default { getToken, decodeAuthToken };
