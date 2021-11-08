d0bc7cdbecd9602d4429fc6c468036d91db6071b
kaixiangtay <taykaixiang97@gmail.com>
2021-11-08 15:52:13 +0800

    Update devops replicas configuration and final Report

--- backend/chat/src/App.js
+++ backend/chat/src/App.js
@@ -11,9 +11,9 @@ const httpServer = createServer(app);
 app.use(cors()); // setup cross origin resource sharing
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+	express.urlencoded({
+		extended: true,
+	})
 );
 
 app.use(express.json());
@@ -21,53 +21,52 @@ app.use(express.json());
 app.use(Router);
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: "https://nusocialife.net",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-  path: "/api/chat/socket.io",
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+	path: "/api/chat/socket.io",
 });
 
 var clients = {};
 
 io.on("connection", (socket) => {
-  console.log(`Connected: ${socket.id}`);
+	console.log(`Connected: ${socket.id}`);
 
-  socket.on("join", (data) => {
-    const roomId = data;
-    console.log(roomId);
-    console.log(`Socket ${socket.id} joining ${roomId}`);
-    socket.join(roomId);
-    // Add user to clients
-    clients[socket.id] = roomId;
+	socket.on("join", (data) => {
+		const roomId = data;
+		console.log(roomId);
+		console.log(`Socket ${socket.id} joining ${roomId}`);
+		socket.join(roomId);
+		// Add user to clients
+		clients[socket.id] = roomId;
 
-    const roomSize = io.sockets.adapter.rooms.get(roomId).size;
+		const roomSize = io.sockets.adapter.rooms.get(roomId).size;
 
-    if (roomSize == 2) {
-      io.to(roomId).emit("profileRequest", "Please send your profile request");
-    }
-  });
+		if (roomSize == 2) {
+			io.to(roomId).emit("profileRequest", "Please send your profile request");
+		}
+	});
 
-  socket.on("profileRetrieval", (data) => {
-    const { roomId, token, profile } = data;
-    io.to(roomId).emit("profileRetrieval", data);
-  });
+	socket.on("profileRetrieval", (data) => {
+		const { roomId, token, profile } = data;
+		io.to(roomId).emit("profileRetrieval", data);
+	});
 
-  socket.on("chat", (data) => {
-    const { roomId, token, message } = data;
-    io.to(roomId).emit("chat", data);
-  });
+	socket.on("chat", (data) => {
+		const { roomId, token, message } = data;
+		io.to(roomId).emit("chat", data);
+	});
 
-  socket.on("disconnect", () => {
-    const roomId = clients[socket.id];
-    console.log(`Disconnected: ${socket.id}`);
-    io.to(roomId).emit("leave", roomId);
-    // Delete user from clients
-    delete clients[socket.id];
-  });
+	socket.on("disconnect", () => {
+		const roomId = clients[socket.id];
+		console.log(`Disconnected: ${socket.id}`);
+		io.to(roomId).emit("leave", roomId);
+		// Delete user from clients
+		delete clients[socket.id];
+	});
 });
 
 httpServer.listen(PORT, () => {
-  console.log(`Server is running at PORT ${PORT}`);
+	console.log(`Server is running at PORT ${PORT}`);
 });
