9e6d3a2c8d463f53153de5ef6205294886681e1b
Marcus Lee Eugene <marcus19787@gmail.com>
2021-11-08 14:34:04 +0800

    Comment out credential for socketio

--- backend/chat/src/App.js
+++ backend/chat/src/App.js
@@ -24,7 +24,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    credentials: true,
+    // credentials: true,
   },
   path: "/api/chat/socket.io",
 });
